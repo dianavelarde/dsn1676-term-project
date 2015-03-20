a0628db54ca397eb6c71d2bddfd8cbccbcdf063a
dianavelarde <van_di09@hotmail.com>
2015-03-19 23:24:41 -0400

    js put in order

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 var $btnMove = $('.btn-move');
 var $book = $('.book');
+var $btnMove2 = $('.btn-move2');
+var $quinoa = $('.quinoa');
 
 $btnMove.on('click', function () {
     $book.toggleClass('js-book');
 });
 
-var $btnMove2 = $('.btn-move2');
-var $quinoa = $('.quinoa');
 
 $btnMove2.on('click', function () {
     $quinoa.toggleClass('js-quinoa');
