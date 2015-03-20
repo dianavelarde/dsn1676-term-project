a3c354c8db7ea681646a94f1c1fbec2c188fd170
dianavelarde <van_di09@hotmail.com>
2015-03-19 23:35:47 -0400

    third button working

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ var $btnMove = $('.btn-move');
 var $book = $('.book');
 var $btnMove2 = $('.btn-move2');
 var $quinoa = $('.quinoa');
+var $btnMove3 = $('.btn-move3');
+var $izze = $('.izze');
 
 $btnMove.on('click', function () {
     $book.toggleClass('js-book');
@@ -10,4 +12,8 @@ $btnMove.on('click', function () {
 
 $btnMove2.on('click', function () {
     $quinoa.toggleClass('js-quinoa');
+});
+
+$btnMove3.on('click', function () {
+    $izze.toggleClass('js-izze');
 });
