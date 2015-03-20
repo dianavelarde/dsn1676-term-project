84e24c8613252cf631bfba2294796560d392f847
dianavelarde <van_di09@hotmail.com>
2015-03-19 23:24:04 -0400

    second button working

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ var $book = $('.book');
 $btnMove.on('click', function () {
     $book.toggleClass('js-book');
 });
+
+var $btnMove2 = $('.btn-move2');
+var $quinoa = $('.quinoa');
+
+$btnMove2.on('click', function () {
+    $quinoa.toggleClass('js-quinoa');
+});
